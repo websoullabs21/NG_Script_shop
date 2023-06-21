597ae82e0e4484b3e21f599a4f8984a2f7ed8501
ngproffesional <ng.proffessionalcreations@gmail.com>
2023-06-21 15:52:25 +0530

    Final file

---
+++ src/components/Heading.js
@@ -0,0 +1,34 @@
+import './Heading.css';
+function Heading (){
+
+
+
+    return(
+
+
+        <div className='*'>
+            <div className='Heading-page'>
+<h1 className='Heading-1'>Scriptshop</h1>
+<p className='Para-1'>Lorem ipsum dolor sit amet, consectetur<br></br> adipiscing elit.
+     Nullam et erat vitae turpis suscipit<br></br> convallis in ac augue. 
+     Sed id quam non neque<br></br> consequat facilisis eu eget risus. 
+     Vestibulum et<br></br> eros non augue interdum fringilla. </p>
+     </div>
+<div className='Signup-Page-box'>
+
+    <h1 className='signup-heading'>Sign Up</h1>
+    <h2 className='writer-1'> Writer</h2>
+    <h2 className='username-1'>User Name</h2>
+    <h2 className='email-1'>Email</h2>
+    <h2 className='password-1'>Password</h2>
+    <h2 className='confirm-password'>Confirm Password</h2>
+    <h2 className='sign-up-buton'>Sign Up</h2>
+    
+
+</div>
+        </div>
+        
+    )
+}
+
+export default Heading;
