0ed9e61e8e9b770f60d19fc72d4be152fec6a3c0
ngproffesional <ng.proffessionalcreations@gmail.com>
2023-06-23 16:23:56 +0530

    ok
    ok

---
+++ src/components/Signup.js
@@ -0,0 +1,34 @@
+import './Signup.css';
+function Signup (){
+
+
+
+    return(
+
+
+        <div className='body-totally'>
+            <div className='Heading-page-le'>
+<h1 className='h1-tag'>Scriptshop</h1>
+<p className='Paragraph-content'>Lorem ipsum dolor sit amet, consectetur<br></br> adipiscing elit.
+     Nullam et erat vitae turpis suscipit<br></br> convallis in ac augue. 
+     Sed id quam non neque<br></br> consequat facilisis eu eget risus. 
+     Vestibulum et<br></br> eros non augue interdum fringilla. </p>
+     </div>
+<div className='Signup-div-11'>
+
+    <h1 className='signup-heading-5'>Sign Up</h1>
+    <h2 className='writer-1-2'> Writer</h2>
+    <h2 className='username-1-2'>User Name</h2>
+    <h2 className='email-1-2'>Email</h2>
+    <h2 className='password-1-2'>Password</h2>
+    <h2 className='confirm-password-2'>Confirm Password</h2>
+    <h2 className='sign-up-buton-2'>Sign Up</h2>
+    
+
+</div>
+        </div>
+        
+    )
+}
+
+export default Signup;
